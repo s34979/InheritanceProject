0ad10322f80b8feedf8c4d95e0a322bfccc7cebf
s34979 <s34979@pjwstk.edu.pl>
2026-04-14 11:20:00 +0200

    Task 1

---
+++ src/Employee.java
@@ -0,0 +1,52 @@
+public class Employee
+{
+  private String employeeId;
+  private String firstName;
+  private String lastName;
+  private double baseSalary;
+  public Employee(String employeeId, String firstName, String lastName, double baseSalary)
+  {
+      this.employeeId=employeeId;
+      this.firstName=firstName;
+      this.lastName=lastName;
+      this.baseSalary=baseSalary;
+  }
+  public final String getEmployeeId()
+  {
+      return employeeId;
+  }
+  public String getLastName()
+  {
+      return lastName;
+  }
+  public double getBaseSalary()
+  {
+      return baseSalary;
+  }
+  public String introduceYourself()
+  {
+      return "I am"+firstName+" "+lastName+".";
+  }
+  @Override
+    public String toString()
+
+  {
+      return "Employee{id='"+ employeeId+ "',+name'"+firstName+" "+lastName+"', salary"+baseSalary+"}";
+
+  }
+  @Override
+    public boolean equals(Object obj)
+  {
+      if(this==obj) return true;
+      if(!(obj instanceof Employee)) return false;
+
+      Employee other=(Employee) obj;
+      return this.employeeId.equals(other.employeeId);
+  }
+  @Override
+    public int hashCode()
+  {
+      return employeeId.hashCode();
+  }
+
+}
